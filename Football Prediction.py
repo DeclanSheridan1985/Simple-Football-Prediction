2796601762ef40908ecd63fc8feef93473ed9c11
Declan Sheridan <sheridan.declan@gmail.com>
2025-07-16 17:56:57 +0100

    added the most recent 6 years of EPL data

--- Football Prediction.py
+++ Football Prediction.py
@@ -1,10 +1,13 @@
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-#English Data - read in the 14 datasets from Football data website
-epl_2021 = pd.read_csv("http://www.football-data.co.uk/mmz4281/2120/E0.csv")
+#English Data - read in the 20 datasets from Football data website
+epl_2425 = pd.read_csv("http://www.football-data.co.uk/mmz4281/2425/E0.csv")
+epl_2324 = pd.read_csv("http://www.football-data.co.uk/mmz4281/2324/E0.csv")
+epl_2223 = pd.read_csv("http://www.football-data.co.uk/mmz4281/2223/E0.csv")
+epl_2122 = pd.read_csv("http://www.football-data.co.uk/mmz4281/2122/E0.csv")
+epl_2021 = pd.read_csv("http://www.football-data.co.uk/mmz4281/2021/E0.csv")
 epl_1920 = pd.read_csv("http://www.football-data.co.uk/mmz4281/1920/E0.csv")
 epl_1819 = pd.read_csv("http://www.football-data.co.uk/mmz4281/1819/E0.csv")
 epl_1718 = pd.read_csv("http://www.football-data.co.uk/mmz4281/1718/E0.csv")
@@ -25,7 +28,7 @@ epl_0506 = pd.read_csv("http://www.football-data.co.uk/mmz4281/0506/E0.csv")
 #concatanate function allows it to be added together even when different columns
 english=pd.concat([epl_0506,epl_0607,epl_0708,epl_0809,epl_0910,epl_1011,
            epl_1112,epl_1213,epl_1314,epl_1415,epl_1516,
-           epl_1617,epl_1718, epl_1819,epl_1920,epl_2021]
+           epl_1617,epl_1718, epl_1819,epl_1920,epl_2021,epl_2122,epl_2223,epl_2324,epl_2425]
 , axis=0, ignore_index=True)
 
 english.to_csv('Football Data.csv')
